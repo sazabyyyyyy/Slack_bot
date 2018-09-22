34281c6b6401ca480afc9c8973e2ac82fda15352
takuya <starfish1138jp@gmail.com>
2018-09-22 10:52:31 +0900

    zaifAPI追加

--- sbbuchiru/plugins/Crypto.py
+++ sbbuchiru/plugins/Crypto.py
@@ -1,20 +1,24 @@
 from slackbot.bot import listen_to
 from slackbot.bot import respond_to
+from zaifapi import ZaifPublicApi
 import pybitflyer
 
+#pybitflyerライブラリを使う。bitflyerライトニングの銘柄から取得
 @listen_to("ビットコイン")
 @listen_to("bitcoin")
-@listen_to("")
 def mention_func1(message):
 
 
  bit=pybitflyer.API()
  btc=bit.ticker(product_code="BTC_JPY")
 
+ #mona&xemはzaifAPIから取得
+ zaif=ZaifPublicApi()
 
-
-
+ xem = zaif.ticker("xem_jpy")
+ mona = zaif.ticker("mona_jpy")
 
  message.send("BTC: "+str(btc["ltp"])+"円")
-
+ message.send("XEM:"+str(xem["last"])+"円")
+ message.send("MONA"+str(mona["last"]) +"円")
 
