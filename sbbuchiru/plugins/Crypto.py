60fd376583cfdc94f79066fa4d219ea646a69784
kaneko <35591275+sazabyyyyyy@users.noreply.github.com>
2018-09-29 02:20:31 +0900

    ver3

--- sbbuchiru/plugins/Crypto.py
+++ sbbuchiru/plugins/Crypto.py
@@ -1,24 +1,27 @@
+import pybitflyer
+from zaifapi import ZaifPublicApi
+
+from bs4 import BeautifulSoup
+import urllib.request as req
+
 from slackbot.bot import listen_to
 from slackbot.bot import respond_to
-from zaifapi import ZaifPublicApi
-import pybitflyer
 
-#pybitflyerライブラリを使う。bitflyerライトニングの銘柄から取得
 @listen_to("ビットコイン")
 @listen_to("bitcoin")
-def mention_func1(message):
+@respond_to('bitcoin')
+@respond_to('ビットコイン')
 
+def mention_func1(message):
 
  bit=pybitflyer.API()
  btc=bit.ticker(product_code="BTC_JPY")
 
- #mona&xemはzaifAPIから取得
  zaif=ZaifPublicApi()
+ xem=zaif.ticker("xem_jpy")
+ mona=zaif.ticker("mona_jpy")
 
- xem = zaif.ticker("xem_jpy")
- mona = zaif.ticker("mona_jpy")
 
  message.send("BTC: "+str(btc["ltp"])+"円")
  message.send("XEM:"+str(xem["last"])+"円")
  message.send("MONA"+str(mona["last"]) +"円")
-
